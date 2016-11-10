0a405a4cf9e40d81b5e94c35aa1707c11f2392ab
himselfv <none@none>
2016-11-10 09:25:17 +0300

    Moved some functions off to a separate unit

--- CBSEnum_Main.pas
+++ CBSEnum_Main.pas
@@ -202,8 +202,8 @@ function IsPackageInList(const AList: TPackageArray; APackage: TPackage): boolea
 function IsPackageInList(const AList: TStringArray; APackageName: string): boolean; inline; overload;
 
 implementation
-uses Clipbrd, XmlDoc, XmlIntf, AccCtrl, AclHelpers, CBSEnum_JobProcessor, TakeOwnershipJob,
-  DecouplePackagesJob, FilenameUtils, AssemblyDbBuilder;
+uses Clipbrd, XmlDoc, XmlIntf, AccCtrl, OsUtils, AclHelpers, CBSEnum_JobProcessor, TakeOwnershipJob,
+  DecouplePackagesJob, FilenameUtils, AssemblyDbBuilder, Generics.Defaults;
 
 {$R *.dfm}
 
@@ -211,53 +211,6 @@ resourcestring
   sCannotOpenCbsRegistry = 'Cannot open registry key for packages. Perpahs '
     +'you''re not running the app with administrator rights? Or the Windows '
     +'version is incompatible.';
-
-function GetSystemDir: string;
-var
-  Buffer: array[0..MAX_PATH] of Char;
-begin
-   GetSystemDirectory(Buffer, MAX_PATH - 1);
-   SetLength(Result, StrLen(Buffer));
-   Result := Buffer;
-end;
-
-function GetWindowsDir: string;
-var
-  Buffer: array[0..MAX_PATH] of Char;
-begin
-   GetWindowsDirectory(Buffer, MAX_PATH - 1);
-   SetLength(Result, StrLen(Buffer));
-   Result := Buffer;
-end;
-
-function StartProcess(const AProgramName, ACommandLine: string): TProcessInformation;
-var startupInfo: TStartupInfo;
-begin
-  FillChar(startupInfo, SizeOf(startupInfo), 0);
-  FillChar(Result, SizeOf(Result), 0);
-  if not CreateProcess(PChar(AProgramName), PChar(ACommandLine),
-    nil, nil, false, 0, nil, nil, startupInfo, Result) then
-    RaiseLastOsError();
-end;
-
-procedure RegeditOpenAndNavigate(const ARegistryPath: string);
-var reg: TRegistry;
-begin
- //That's the only damn way
- //Well, there's also regjump.exe from SysInternals but it shows EULA and it's
- //a dependency.
- //It can also be done directly using UI automation.
-  reg := TRegistry.Create;
-  try
-    reg.RootKey := HKEY_CURRENT_USER;
-    if not reg.OpenKey('Software\Microsoft\Windows\CurrentVersion\Applets\Regedit', true) then
-      raise Exception.Create('Cannot point regedit at a key.');
-    reg.WriteString('LastKey', ARegistryPath);
-  finally
-    FreeAndNil(reg);
-  end;
-  StartProcess(GetWindowsDir()+'\regedit.exe', 'regedit.exe');
-end;
 
 
 function IsPackageInList(const AList: TPackageArray; APackage: TPackage): boolean; inline; overload;
---
+++ OsUtils.pas
@@ -0,0 +1,63 @@
+unit OsUtils;
+
+interface
+uses SysUtils, Windows;
+
+function GetSystemDir: string;
+function GetWindowsDir: string;
+
+function StartProcess(const AProgramName, ACommandLine: string): TProcessInformation;
+procedure RegeditOpenAndNavigate(const ARegistryPath: string);
+
+
+implementation
+uses Registry;
+
+function GetSystemDir: string;
+var
+  Buffer: array[0..MAX_PATH] of Char;
+begin
+   GetSystemDirectory(Buffer, MAX_PATH - 1);
+   SetLength(Result, StrLen(Buffer));
+   Result := Buffer;
+end;
+
+function GetWindowsDir: string;
+var
+  Buffer: array[0..MAX_PATH] of Char;
+begin
+   GetWindowsDirectory(Buffer, MAX_PATH - 1);
+   SetLength(Result, StrLen(Buffer));
+   Result := Buffer;
+end;
+
+function StartProcess(const AProgramName, ACommandLine: string): TProcessInformation;
+var startupInfo: TStartupInfo;
+begin
+  FillChar(startupInfo, SizeOf(startupInfo), 0);
+  FillChar(Result, SizeOf(Result), 0);
+  if not CreateProcess(PChar(AProgramName), PChar(ACommandLine),
+    nil, nil, false, 0, nil, nil, startupInfo, Result) then
+    RaiseLastOsError();
+end;
+
+procedure RegeditOpenAndNavigate(const ARegistryPath: string);
+var reg: TRegistry;
+begin
+ //That's the only damn way
+ //Well, there's also regjump.exe from SysInternals but it shows EULA and it's
+ //a dependency.
+ //It can also be done directly using UI automation.
+  reg := TRegistry.Create;
+  try
+    reg.RootKey := HKEY_CURRENT_USER;
+    if not reg.OpenKey('Software\Microsoft\Windows\CurrentVersion\Applets\Regedit', true) then
+      raise Exception.Create('Cannot point regedit at a key.');
+    reg.WriteString('LastKey', ARegistryPath);
+  finally
+    FreeAndNil(reg);
+  end;
+  StartProcess(GetWindowsDir()+'\regedit.exe', 'regedit.exe');
+end;
+
+end.
